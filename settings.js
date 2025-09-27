9020faefed22769dcd30dbdbe835c9d7f9e9e175
Teestar001 <86662606+Teestar001@users.noreply.github.com>
2025-09-27 23:24:03 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '𝐆𝐎𝐃𝐒𝐙𝐄𝐀𝐋 𝐗𝐌𝐃',
-  author: '‎Gods Zeal †',
-  botName: "𝐆𝐎𝐃𝐒𝐙𝐄𝐀𝐋 𝐗𝐌𝐃",
-  botOwner: 'Gods Zeal †', // Your name
-  ownerNumber: '2348089336992', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Outlet', // packname
+  author: '‎𝕏',
+  botName: "Outlet 𝕏",
+  botOwner: '𝕏', // Your name
+  ownerNumber: '2347043031692', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
